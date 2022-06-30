baae3facc7279a1079fd9068fe77652cc7506f9d
PickPlugins <32812881+pickplugins@users.noreply.github.com>
2022-06-30 12:55:26 +0600

    .gitignore Fixed

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1ae93145a0719342cd54');

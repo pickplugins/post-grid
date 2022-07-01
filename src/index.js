9eae2606becc36f165d59e165845a1cb1ac0df0a
PickPlugins <32812881+pickplugins@users.noreply.github.com>
2022-07-01 12:37:26 +0600

    post tags added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ window.PostGridPluginData = [];
 import './blocks/post-grid'
 import './blocks/post-title'
 import './blocks/post-categories'
+import './blocks/post-tags'
+
 import './blocks/post-exc'
 
 // import './blocks/paragraph'
